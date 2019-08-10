8eaecb7b6cbc65b6514d4c56c3245ff6e24f94cc
Kaleado <kaleadoskope@hotmail.com>
2019-08-10 21:24:00 +1000

    Fixed again

--- backend/src/submission.ts
+++ backend/src/submission.ts
@@ -5,6 +5,11 @@ import {
     ChallengeId } from "../../shared";
 import { RedisClient } from "redis";
 
+function getTestsPassed(code: string, language: Language, id: ChallengeId): number {
+    // Run tests here.
+    return 0;
+}
+
 export async function makeSubmission(req, res, dbClient: RedisClient): Promise<SubmissionResponse | BackendError> {
     return {
         testsPassed: 12,
