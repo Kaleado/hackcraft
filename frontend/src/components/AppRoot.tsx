4ced86cf1dd54e761a892ae7ef02140af6731502
Emmet Murray <e.minor.murray@gmail.com>
2019-08-10 12:48:47 +1000

    Started login

--- frontend/src/components/AppRoot.tsx
+++ frontend/src/components/AppRoot.tsx
@@ -1,17 +1,37 @@
 import * as React from "react";
+import * as globals from "../../../shared/globals";
+import "../style.css";
+import { LoginPanel } from "./login/LoginPanel";
 
-export interface IAppRootProps {
-  compiler: string;
-  framework: string;
+export interface IAppRootProps { }
+
+interface IAppRootState {
+  loggedIn: boolean;
+  userName?: string;
 }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class AppRoot extends React.Component<IAppRootProps, {}> {
+export class AppRoot extends React.Component<IAppRootProps, IAppRootState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+    };
+  }
+
+  performLogin(un: string, pw: string) {
+    fetch()
+  }
+
   render() {
-    return <div>
-      <h1>Hi from {this.props.compiler} and {this.props.framework}!</h1>
-      <p>This is a paragraph</p>
-    </div>;
+    if (!this.state.loggedIn) {
+      return <div className="login-panel-position">
+        <LoginPanel></LoginPanel>
+      </div>;
+    }
+
+    return <div></div>
   }
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import "./style.css";
-
 import { AppRoot } from "./components/AppRoot";
 
 ReactDOM.render(
-    <AppRoot compiler="TypeScript" framework="React" />,
+    <AppRoot />,
     document.getElementById("main"),
 );
