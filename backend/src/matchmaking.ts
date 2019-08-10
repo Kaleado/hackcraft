e82b9dd66372537820ae360d5595dda77b456246
Kaleado <kaleadoskope@hotmail.com>
2019-08-10 15:34:19 +1000

    Corrected signature of statrMatchmaking

--- backend/src/matchmaking.ts
+++ backend/src/matchmaking.ts
@@ -1,6 +1,7 @@
 import { StartMatchmakingResponse, BackendError } from "../../shared";
+import { RedisClient } from "redis";
 
-export async function startMatchmaking() : Promise<StartMatchmakingResponse | BackendError> {
+export async function startMatchmaking(req, res, dbClient: RedisClient) : Promise<StartMatchmakingResponse | BackendError> {
     return {
         matchId: 0 // dummy response for now
     };
